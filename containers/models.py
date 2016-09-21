4b351f39ea5f0f6656754d77e2162f20921a1502
aluizio <aluizio@dcc.ufrj.br>
2016-09-21 16:11:19 -0300

    Change the structure of the project

--- containers/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-
-# Create your models here.
-
-class Container(models.Model):
-    name = models.CharField(max_length=255, null=False, blank=False, unique=True)
-    user = models.ForeignKey(User, related_name='container')
---
+++ institution/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class InstitutionProfile(models.Model):
+    name = models.CharField(max_length=255, null=False, blank=False)
+    user = models.OneToOneField(User, related_name='institution_profile')
+
+class Service(models.Model):
+    url = models.CharField(max_length=1000, null=False, blank=False)
+    institution = models.ForeignKey(InstitutionProfile, related_name='services')
+
+class Container(models.Model):
+    name = models.CharField(max_length=255, null=False, blank=False, unique=True)
+    institution = models.ForeignKey(InstitutionProfile, related_name='container')
+
+    @property
+    def name_api(self):
+        return self.name + "_api"
+
+    @property
+    def name_geonode(self):
+        return self.name + "_geonode"

430c2461a157e9c928328310e55e878190efa01d
aluizio <aluizio@dcc.ufrj.br>
2016-09-21 19:26:22 -0300

    Create the first version of main page

--- ServiceManager/settings.py
+++ ServiceManager/settings.py
@@ -23,6 +23,8 @@ STATICFILES_DIRS = [
     os.path.join(PROJECT_ROOT, "static"),
 ]
 
+MEDIA_ROOT = os.path.join(BASE_DIR, "media_root/")
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
@@ -136,3 +138,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/idehco3/servicemanager/static/'
+MEDIA_URL = '/idehco3/servicemanager/media/'
--- institution/models.py
+++ institution/models.py
@@ -4,7 +4,10 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class InstitutionProfile(models.Model):
-    name = models.CharField(max_length=255, null=False, blank=False)
+    name = models.CharField(max_length=1000, null=False, blank=False)
+    initials = models.CharField(max_length=255, null=True, blank=False)
+    description = models.CharField(max_length=2000, null=True, blank=False)
+    image = models.ImageField(upload_to='images/institutions/', null=True, blank=False)
     user = models.OneToOneField(User, related_name='institution_profile')
 
 class Service(models.Model):

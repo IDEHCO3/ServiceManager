48ae98c714ea013a18fa5ac7789133aa1b04c473
aluizio <aluizio777@gmail.com>
2016-09-30 19:46:44 -0300

    Fixed urls names

--- ServiceManager/urls.py
+++ ServiceManager/urls.py
@@ -14,10 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
 from django.conf.urls import include, url
-from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^servicecatalog/', include('pages.urls', namespace='pages')),
-    url(r'^institutions/', include('institution.urls', namespace='institutions'))
+    url(r'^idehco3/servicemanager/', include('pages.urls', namespace='pages')),
+    url(r'^idehco3/servicemanager/institutions/', include('institution.urls', namespace='institutions'))
 ]

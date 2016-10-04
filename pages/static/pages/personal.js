b5b177e3a851f0971078b3cf675ec009f71434de
aluizio <aluizio777@gmail.com>
2016-10-04 19:03:11 -0300

    Starting to use angulajs in personal page

--- pages/static/pages/personal.js
+++ pages/static/pages/personal.js
@@ -1,7 +1,29 @@
 (function(){
-    var personal_app = angular.module('personalApp',[]);
+    var app = angular.module('personalApp',[])
+        .config(function ($interpolateProvider) {
+            $interpolateProvider.startSymbol('{$');
+            $interpolateProvider.endSymbol('$}');
+        });
 
-    personal_app.controler('personalController', ['$scope', '$http', function($scope, $http){
+    var url_base = '/idehco3/servicemanager/';
+    app.controller('personalController', ['$scope', '$http', function($scope, $http){
         $scope.institution = null;
+        $http.get(url_base+'institutions/ibge/')
+            .success(function(data){
+                console.log("data: ",data);
+                $scope.institution = data;
+                $http.get(url_base+'institutions/ibge/services')
+                    .success(function(data){
+                        $scope.institution.links = data;
+                        console.log("data2: ", data);
+                    })
+                    .error(function(data){
+                        $scope.institution.links = [];
+                        console.log("error2: ", data);
+                    });
+            })
+            .error(function(data){
+                console.log("error: ",data);
+            });
     }]);
-});
+})();

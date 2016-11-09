9d6e6538d1ac90297843690eec27f14e8b16ff3d
aluizio <aluizio777@gmail.com>
2016-11-09 20:23:42 -0200

    The Container model was improved.

--- institution/models.py
+++ institution/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+import os
+import shutil
 # Create your models here.
 
 class InstitutionProfile(models.Model):
@@ -21,8 +23,8 @@ class Link(models.Model):
 #     link = models.ForeignKey(Link, related_name='services')
 
 class Container(models.Model):
+    containers = "containers"
     name = models.CharField(max_length=255, null=False, blank=False, unique=True)
-    folderOfDatabase = models.CharField(max_length=1000)
     institution = models.ForeignKey(InstitutionProfile, related_name='container')
 
     @property
@@ -32,3 +34,51 @@ class Container(models.Model):
     @property
     def name_geonode(self):
         return self.name + "_geonode"
+
+    @property
+    def container_folder(self):
+        if self.id is not None:
+            return os.getcwd()+"/"+Container.containers+"/"+str(self.id)
+        else:
+            return None
+
+    @property
+    def geonode_folder(self):
+        if self.container_folder is not None:
+            return self.container_folder+"/geonode"
+        else:
+            return None
+
+    @property
+    def api_folder(self):
+        if self.container_folder is not None:
+            return self.container_folder+"/api"
+        else:
+            return None
+
+    def create_folders(self):
+        if not os.path.exists(os.getcwd()+"/"+Container.containers):
+            os.mkdir(os.getcwd()+"/"+Container.containers)
+
+        if self.container_folder is not None and not os.path.exists(self.container_folder):
+            os.mkdir(self.container_folder)
+        if self.geonode_folder is not None and not os.path.exists(self.geonode_folder):
+            os.mkdir(self.geonode_folder)
+        if self.api_folder is not None and not os.path.exists(self.api_folder):
+            os.mkdir(self.api_folder)
+
+
+    def create_containers(self):
+        self.create_folders()
+
+    def delete_containers(self):
+        if self.container_folder is not None and os.path.exists(self.container_folder):
+            shutil.rmtree(self.container_folder)
+
+    def save(self):
+        super(Container, self).save()
+        self.create_containers()
+
+    def delete(self):
+        self.delete_containers()
+        super(Container, self).delete()
